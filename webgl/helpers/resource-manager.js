d94674328f3ce6be2ce95f2bbf1ee1bcba5d85be
Patrick Cozzi <pjcozzi@gmail.com>
2012-11-06 17:08:25 -0500

    Fixed malformed HTTP request header

--- webgl/helpers/resource-manager.js
+++ webgl/helpers/resource-manager.js
@@ -546,7 +546,7 @@ var global = window;
                     xhr.setRequestHeader("Range", header);
                 }
                 //if this is not specified, 1 "big blob" scenes fails to load.
-//                xhr.setRequestHeader("If-Modified-Since", "Sat, 1 Jan 1970 00:00:00 GMT");
+                xhr.setRequestHeader("If-Modified-Since", "Sat, 01 Jan 1970 00:00:00 GMT");
                 xhr.onload = function(e) {
                     if ((this.status == 200) || (this.status == 206)) {
                         self._resourcesBeingProcessedCount--;
