63e932907e3f0c834c8668d04f03ddb6eabf78d1
Fabrice Robinet <fabricerobinet@me.com>
2014-05-13 20:30:35 -0700

    Update models for v0.6

--- model/wine/wine2FS.glsl
+++ model/wine/wine2FS.glsl
@@ -2,7 +2,6 @@ precision highp float;
 varying vec3 v_normal;
 uniform vec4 u_ambient;
 uniform vec4 u_diffuse;
-uniform float u_transparency;
 void main(void) {
 vec3 normal = normalize(v_normal);
 vec4 color = vec4(0., 0., 0., 0.);
@@ -12,6 +11,6 @@ ambient = u_ambient;
 diffuse = u_diffuse;
 diffuse.xyz *= max(dot(normal,vec3(0.,0.,1.)), 0.);
 color.xyz += diffuse.xyz;
-color = vec4(color.rgb * diffuse.a, diffuse.a * u_transparency);
+color = vec4(color.rgb * diffuse.a, diffuse.a);
 gl_FragColor = color;
 }
