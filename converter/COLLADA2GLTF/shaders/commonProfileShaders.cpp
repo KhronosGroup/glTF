02e492e071c385f936de67c4dd9da890cc0e9f96
Fabrice Robinet <fabricerobinet@me.com>
2014-06-09 11:12:50 -0700

    https://github.com/KhronosGroup/glTF/issues/258

--- converter/COLLADA2GLTF/shaders/commonProfileShaders.cpp
+++ converter/COLLADA2GLTF/shaders/commonProfileShaders.cpp
@@ -639,6 +639,9 @@ namespace GLTF
                 }
             } else {
                 shader->addUniform(symbol, type, count, forcesAsAnArray);
+                if ((count > 1) || forcesAsAnArray) {
+                    parameter->setUnsignedInt32(kCount, count);
+                }
             }
             
             return true;
