0fa6b864ef0ca1282264d249c127bee2b82fd77b
Tom Fili <tfili@agi.com>
2014-07-09 17:28:11 -0400

    Fixed crash caused by freeing and array twice.

--- converter/COLLADA2GLTF/convert/meshConverter.cpp
+++ converter/COLLADA2GLTF/convert/meshConverter.cpp
@@ -478,10 +478,6 @@ namespace GLTF
             __AppendIndices(cvtPrimitive, primitiveIndicesVector, binormalIndices, TEXBINORMAL, 0);
         }
         
-        if (verticesCountArray) {
-            free(verticesCountArray);
-        }
-        
         //TODO:refactor
         if (openCOLLADAMeshPrimitive->hasTangentIndices()) {
             unsigned int triangulatedIndicesCount = 0;
