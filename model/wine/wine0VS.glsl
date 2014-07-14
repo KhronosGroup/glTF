584b8c89857906aee1a3eb8a8a6806059b2b80bf
Fabrice Robinet <fabricerobinet@me.com>
2014-07-14 13:58:37 -0700

    convert files to v0.7

--- model/wine/wine0VS.glsl
+++ model/wine/wine0VS.glsl
@@ -9,7 +9,7 @@ attribute vec2 a_texcoord0;
 varying vec2 v_texcoord0;
 void main(void) {
 vec4 pos = u_modelViewMatrix * vec4(a_position,1.0);
-v_normal = normalize(u_normalMatrix * a_normal);
+v_normal = u_normalMatrix * a_normal;
 v_texcoord0 = a_texcoord0;
 gl_Position = u_projectionMatrix * pos;
 }
