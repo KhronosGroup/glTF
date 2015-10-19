3f25d7ffc11a6cf5636cae54e025328cd4c47290
Patrick Cozzi <pjcozzi@gmail.com>
2015-10-19 07:51:42 -0400

    Remove sample models

--- sampleModels/rambler/Rambler0FS.glsl
+++
@@ -1,8 +0,0 @@
-precision highp float;
-void main(void) {
-vec4 color = vec4(0., 0., 0., 0.);
-vec4 diffuse = vec4(0., 0., 0., 1.);
-color.xyz += diffuse.xyz;
-color = vec4(color.rgb * diffuse.a, diffuse.a);
-gl_FragColor = color;
-}
