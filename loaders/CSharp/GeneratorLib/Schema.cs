8f91d30d6f8dcc20aa1e2ed28d2641a22c0f6718
MorituriTeSalutant <nerdfencer@gmail.com>
2015-11-19 15:21:03 -0800

    rearrange properties in alphabetical order

--- loaders/CSharp/GeneratorLib/Schema.cs
+++ loaders/CSharp/GeneratorLib/Schema.cs
@@ -10,24 +10,16 @@ namespace GeneratorLib
 {
     public class Schema
     {
-        public string ResolvedType { get; set; }
+        public Schema AdditionalItems { get; set; }
 
-        [JsonProperty("__ref__")]
-        public string ReferenceType { get; set; }
+        public Dictionary<string, string> Dependencies { get; set; }
 
-        public string Title { get; set; }
-
-        [JsonConverter(typeof(ArrayOfTypeReferencesConverter))]
-        public TypeReference[] Type { get; set; }
+        public object Default { get; set; }
 
         public string Description { get; set; }
 
-        [JsonConverter(typeof (TypeReferenceConverter))]
-        public TypeReference Extends { get; set; }
-
-        public Dictionary<string, Schema> Properties { get; set; }
-
-        public Dictionary<string, Schema> PatternProperties { get; set; }
+        [JsonProperty("gltf_detailedDescription")]
+        public string DetailedDescription { get; set; }
 
         [JsonProperty("additionalProperties")]
         public Schema DictionaryValueType { get; set; }
@@ -36,45 +28,67 @@ namespace GeneratorLib
 
         public int DivisibleBy { get; set; }
 
-        public Dictionary<string, string> Dependencies { get; set; }
-        
+        [JsonConverter(typeof(TypeReferenceConverter))]
+        public TypeReference Extends { get; set; }
+
+        public object Enum { get; set; }
+
+        public bool ExclusiveMaximum { get; set; }
+
+        public bool ExclusiveMinimum { get; set; }
+
+        public string Format { get; set; }
+
         public Schema Items { get; set; }
 
         public string Id { get; set; }
 
-        public Schema AdditionalItems { get; set; }
+        public int? MaxItems { get; set; }
+
+        public int MaxLength { get; set; }
+
+        public object Maximum { get; set; }
+
+        public int? MinItems { get; set; }
+
+        public int MinLength { get; set; }
+
+        public object Minimum { get; set; }
+
+        public Dictionary<string, Schema> PatternProperties { get; set; }
 
         public string Pattern { get; set; } //format regex?
 
-        public string Format { get; set; }
+        public Dictionary<string, Schema> Properties { get; set; }
 
-        public object Default { get; set; }
+        [JsonProperty("__ref__")]
+        public string ReferenceType { get; set; }
 
-        [JsonProperty("gltf_detailedDescription")]
-        public string DetailedDescription { get; set; }
+        public bool Required { get; set; }
+
+        public string ResolvedType { get; set; }
+
+        public string Title { get; set; }
+
+        [JsonConverter(typeof(ArrayOfTypeReferencesConverter))]
+        public TypeReference[] Type { get; set; }
+
+        public bool UniqueItems { get; set; }
 
         [JsonProperty("gltf_webgl")]
         public string WebGl { get; set; }
 
-        public int? MinItems { get; set; }
 
-        public int? MaxItems { get; set; }
 
-        public object Enum { get; set; }
 
-        public object Minimum { get; set; }
 
-        public object Maximum { get; set; }
 
-        public bool Required { get; set; }
 
-        public bool UniqueItems { get; set; }
 
-        public bool ExclusiveMinimum { get; set; }
 
-        public int MinLength { get; set; }
 
-        public int MaxLength { get; set; }
+
+
     }
 
     public class TypeReference
