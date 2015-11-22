bfe10b6f787130225f18a5a05dc9ba9eb6db3ba4
MorituriTeSalutant <nerdfencer@gmail.com>
2015-11-22 06:48:30 -0800

    Fix a bug caused by refactoring in the generator program

--- loaders/CSharp/Generator/Program.cs
+++ loaders/CSharp/Generator/Program.cs
@@ -11,7 +11,7 @@ namespace Generator
             generator.ParseSchemas();
             generator.ExpandSchemaReferences();
             generator.EvaluateInheritance();
-            generator.CSharpCodeGen(Path.GetFullPath(@"..\..\..\glTFLoader\ParserSchema"));
+            generator.CSharpCodeGen(Path.GetFullPath(@"..\..\..\glTFLoader\Schema"));
         }
     }
 }
