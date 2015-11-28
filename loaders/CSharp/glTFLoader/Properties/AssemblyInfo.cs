4322d33c4e4477aebc105d121e69852c95ab3d57
yukicode <yolandaapply@gmail.com>
2015-11-27 17:36:03 -0800

    Add spec  for nuget, Add automatic nuget package generation.

--- loaders/CSharp/glTFLoader/Properties/AssemblyInfo.cs
+++ loaders/CSharp/glTFLoader/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("glTFLoader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# reference loader for glTF")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Matthew and Xueru McMullan")]
 [assembly: AssemblyProduct("glTFLoader")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
